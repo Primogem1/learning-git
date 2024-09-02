ab3978f4ab697c8ddb9c53b370388b7ced320f4c
Kate Khreshkova <katahrnskova@gmail.com>
2024-09-03 10:16:47 +1200

    added comment

--- src/main/java/by/katekhreshkova/firstgitproject/FirstGit.java
+++ src/main/java/by/katekhreshkova/firstgitproject/FirstGit.java
@@ -2,6 +2,7 @@ package by.katekhreshkova.firstgitproject;
 
 public class FirstGit {
     public static void main(String[] args) {
+        //comment
         System.out.println("Hello git");
     }
 }
