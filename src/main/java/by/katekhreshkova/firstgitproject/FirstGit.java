3f7f84e34781a35dcf7b1febda34ae49bd7094d7
Primogem1 <164609543+Primogem1@users.noreply.github.com>
2024-09-03 10:26:14 +1200

    Update FirstGit.java
    Added !

--- src/main/java/by/katekhreshkova/firstgitproject/FirstGit.java
+++ src/main/java/by/katekhreshkova/firstgitproject/FirstGit.java
@@ -2,6 +2,6 @@ package by.katekhreshkova.firstgitproject;
 
 public class FirstGit {
     public static void main(String[] args) {
-        System.out.println("Hello git");
+        System.out.println("Hello git!");
     }
 }
