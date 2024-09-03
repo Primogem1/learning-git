e78bc0b608b0aed76ba0e7d5501dd769e63f898f
Kate Khreshkova <katahrnskova@gmail.com>
2024-09-03 14:22:10 +1200

    hello git develope

--- src/main/java/by/katekhreshkova/firstgitproject/FirstGit.java
+++ src/main/java/by/katekhreshkova/firstgitproject/FirstGit.java
@@ -2,11 +2,8 @@ package by.katekhreshkova.firstgitproject;
 
 public class FirstGit {
     public static void main(String[] args) {
-        System.out.println("Hello git!");
-<<<<<<< HEAD
+        System.out.println("Hello git develope !");
         System.out.println("I am happy");
-=======
         System.out.println("It is terribly difficult");
->>>>>>> 0137e259daa90de4bd2fbba9432f25c2f066930f
     }
 }
